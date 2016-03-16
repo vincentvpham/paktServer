bb56aee86369740b07e196f527f43daef9636bfa
Deniz Mekik <denizmekik@gmail.com>
2016-03-16 10:56:29 -0700

    remove comments

--- controllers/paktController.js
+++ controllers/paktController.js
@@ -1,8 +1,3 @@
-// var db = require('../utils/db.js');
-// var Pakt = require('../models/paktModel.js').Pakt;
-// var User_Pakt = require('../models/paktModel.js').User_Pakt;
-// var Sequelize = require('sequelize');
-// var UserQuery = require('../queries/UserQueries');
 var PaktQuery = require('../queries/paktQueries');
 module.exports = {
   getAllPakts: function (req, res) {
